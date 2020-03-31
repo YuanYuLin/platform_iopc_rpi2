b89a142034e8b4fcaa5512b34c10458343755f9a
yyl <yuanyu.lin@gmail.com>
2020-03-31 16:20:16 +0800

    MAJOR_NUM=1 MINOR_NUM=4 AUX_NUM=0 update package.

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -5,16 +5,14 @@ pkg_path = ""
 output_dir = ""
 arch = ""
 output_platform_dir = ""
-src_image_cfg = ""
-dao_file = "dao.ini"
 pkg_tarball = "firmware.tar.xz"
 pkg_tarball_dir = "firmware"
+dao_files = ["dao"]
 
 def set_global(args):
     global pkg_path
     global output_dir
     global arch
-    global src_image_cfg
     global output_platform_dir
     global install_platform_dao
     global dao_script
@@ -24,7 +22,6 @@ def set_global(args):
     pkg_path = args["pkg_path"]
     output_dir = args["output_path"]
     arch = ops.getEnv("ARCH_ALT")
-    src_image_cfg = ops.path_join(pkg_path, dao_file)
     output_platform_dir = ops.path_join(iopc.getOutputRootDir(), "platform")
     dao_script = ops.path_join(output_platform_dir, "dao.py")
     pkg_tarball = ops.path_join(pkg_path, pkg_tarball)
@@ -41,8 +38,11 @@ def MAIN_ENV(args):
 def MAIN_EXTRACT(args):
     set_global(args)
 
-    ops.copyto(src_image_cfg, ops.path_join(output_platform_dir, dao_file))
-    ops.copyto(ops.path_join(pkg_path, dao_file), output_dir)
+    ops.mkdir(output_platform_dir)
+    for ini in dao_files:
+        dao_file = ops.path_join(pkg_path, ini + ".ini")
+        ops.copyto(dao_file, output_platform_dir)
+        ops.copyto(dao_file, output_dir)
 
     ops.unTarXz(pkg_tarball, output_dir)
 
@@ -66,8 +66,12 @@ def MAIN_BUILD(args):
     set_global(args)
 
     if install_platform_dao == True :
-        CMD=['python', dao_script, dao_file]
-        ops.execCmd(CMD, output_dir, False)
+        for ini in dao_files:
+            dao_file = ops.path_join(output_platform_dir, ini + ".ini")
+            dao_bin = ops.path_join(output_platform_dir, ini + ".bin")
+            CMD=['python', dao_script, dao_file, dao_bin]
+            print CMD
+            ops.execCmd(CMD, output_dir, False)
 
     return False
 
@@ -75,9 +79,22 @@ def MAIN_INSTALL(args):
     set_global(args)
 
     if install_platform_dao == True :
-        ops.copyto(ops.path_join(output_dir, 'db_init.bin'), iopc.getOutputRootDir())
-        ops.copyto(ops.path_join(output_dir, 'img_header.bin'), iopc.getOutputRootDir())
+        #ops.copyto(ops.path_join(output_dir, 'db_init.bin'), iopc.getOutputRootDir())
+        #ops.copyto(ops.path_join(output_dir, 'img_header.bin'), iopc.getOutputRootDir())
         iopc.installBin(args["pkg_name"], ops.path_join(output_dir, "db_init.inc"), 'include/platform')
+
+    return False
+
+def MAIN_SDKENV(args):
+    set_global(args)
+    return False
+
+def MAIN_SDKENV(args):
+    set_global(args)
+
+    cflags = ""
+    cflags += " -I" + ops.path_join(iopc.getSdkPath(), 'usr/include/platform')
+    iopc.add_includes(cflags)
 
     return False
 
